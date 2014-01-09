ef911b2fb4c9d03e8af165cc78240a634c22a33c
Nicolas Winslow <nicwins@gmail.com>
2014-01-08 20:50:33 -0800

    fix(magit.el): open diff all commit types
    Opens the diff window on all commit types (amend, extend etc.) not just
    regular commit.  Also kills the diff buffer, not just its window.

--- setup-magit.el
+++ setup-magit.el
@@ -11,23 +11,31 @@
 (set-face-foreground 'diff-added "#fff")
 (set-face-background 'diff-removed "#ac2925")
 
+(add-hook 'server-switch-hook (lambda ()
+                                (if (derived-mode-p 'git-commit-mode)
+                                    (progn (delete-other-windows)
+                                           (split-window-right)
+                                           (switch-to-buffer-other-window "*magit-diff*")
+                                           (other-window 1)))))
+
+(add-hook 'magit-refresh-file-buffer-hook (lambda () (kill-buffer "*magit-diff*")))
+
+(defadvice magit-commit-internal (before magit-commit-diff activate)
+  "Add the diff buffer to the commit view."
+  (magit-diff-staged))
+
 (defadvice magit-status (around magit-fullscreen activate)
   "Set magit status to full-screen."
   (window-configuration-to-register :magit-fullscreen)
   ad-do-it
   (delete-other-windows))
 
-(defadvice magit-commit (around magit-commitdiff activate)
-  "Add the diff buffer to the commit view."
-  (delete-other-windows)
-  ad-do-it
-  (magit-diff-staged))
-
 (defun magit-quit-session ()
   "Restore the previous window configuration and kill the magit buffer."
   (interactive)
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
+
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (provide 'setup-magit)
