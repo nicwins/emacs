a42d36744fa1e6349f97ba296788e7ec8aed5445
Nicolas Winslow <nicwins@gmail.com>
2014-01-21 19:53:08 -0800

    fix(js2-mode): add breeze and moment as globals

--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -1,5 +1,5 @@
 (setq-default js2-global-externs
-              '("angular" "inject" "describe" "expect" "it" "beforeEach" "afterEach" "$" "_" "JSON" "jasmine" "spyOn" "module" "Class"))
+              '("angular" "inject" "describe" "expect" "it" "beforeEach" "afterEach" "$" "_" "JSON" "jasmine" "spyOn" "module" "breeze" "moment"))
 (setq-default js2-idle-timer-delay 0.3)
 (setq-default js2-mirror-mode nil)
 (setq-default js2-strict-inconsistent-return-warning nil)
